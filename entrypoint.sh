5f62c805e8fcd2743df4ef8f92162ce136aae0e9
appotry <andycrusoe@gmail.com>
2022-03-10 21:33:48 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 #chown tor:tor /var/log/tor
 #touch /var/log/tor/notices.log
 chown tor:nogroup /var/log/tor/notices.log
+ls -alh /var/log/tor/notices.log
 
 if [ ! -f /app/etc/torrc ]; then 
     echo "cp torrc"
